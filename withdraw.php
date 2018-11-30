39bffccfb57ac673ca77128716bf072ab5dbc2db
K-Niz <fubuki126@gmail.com>
2018-12-01 02:22:43 +0700

    with

---
+++ withdraw.php
@@ -0,0 +1,18 @@
+<?php
+    session_start();
+    require('php/getConnection.php');
+    $conn = getConnection();
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    $username = $_SESSION['Username']; 
+    $credit = mysqli_real_escape_string($conn, $_GET['cred']);
+    //echo "$credit";
+    $query = "UPDATE User SET Credit = Credit - '$credit' WHERE UserID = '$username'";
+    $result = $conn->query($query);
+    //echo "Top-up done !!! Please wait...";
+    //header( "refresh:1; url=creditmanagement.php" );
+    echo "<script> alert('Withdraw complete'); window.location = 'creditmanagement.php';</script>";
+    mysqli_close($conn);
+?>
